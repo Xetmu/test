4a998de8f7d53d0a1d60537ddecb24402ec4c5e3
Xetmu <72429945+Xetmu@users.noreply.github.com>
2025-08-01 02:40:21 +1000

    fix_задание.java

--- задание.java
+++ задание.java
@@ -39,67 +39,67 @@ public class Main
 	        	throw new ArithmeticException();
 		}
 	      
-        if(words.length==1)
-        {
-        	System.err.println("throws Exception //т.к. строка не является математической операцией"); 
-        	throw new ArithmeticException();
-        }
-        
-        if(words.length!=3)
-	        for(int i = 0; i< words.length; i++)
+	        if(words.length==1)
 	        {
-			if((i%2)==0)
-		        {
-		        	Integer k = Integer.valueOf(words[i]);
-		        	if(!(-1<k&&k<11)){
-		        	System.err.println("Некорректный ввод, на месте числа иной символ"); 
-		                throw new ArithmeticException();
-		            	}
-		        }
-		        if((i%2)==1)
-		        {
-		        	if(!((words[i].equals("+")||(words[i].equals("-"))||(words[i].equals("*"))||(words[i].equals("/"))))){
-		        		System.err.println(i + words[i] + "Некорректный ввод, на месте знака операции иной символ"); 
-		                	throw new ArithmeticException();
-		            	}
-		         }
-		         if(i==(words.length-1)){
-		         	System.err.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)"); 
-		                throw new ArithmeticException();
-		         }
-	        }
-        try {
-	        Integer i1 = Integer.valueOf(words[0]);
-	        Integer i2 = Integer.valueOf(words[2]);
-	        if(i1<0 || i1>10 || i2<0 || i2>10)
-	        {
-			System.err.println("Неправильный формат строки!");  
+	        	System.err.println("throws Exception //т.к. строка не является математической операцией"); 
 	        	throw new ArithmeticException();
 	        }
-	        switch(words[1]){
-	        	case "+":
-	               		return (i1+i2);
-	                
-	        	case "-":
-	               		return (i1-i2);
-
-	           	case "/":
-	               		if(i2==0)
-	                    		System.err.println("Делить на ноль нельзя!");  
-	               			return (i1/i2);     
-	           	case "*":
-	               		return (i1*i2);    
-	           	default:
-	           		System.err.println("Неправильный формат строки!");  
-	           		throw new ArithmeticException();   
+	        
+	        if(words.length!=3)
+		        for(int i = 0; i< words.length; i++)
+		        {
+				if((i%2)==0)
+			        {
+			        	Integer k = Integer.valueOf(words[i]);
+			        	if(!(-1<k&&k<11)){
+			        	System.err.println("Некорректный ввод, на месте числа иной символ"); 
+			                throw new ArithmeticException();
+			            	}
+			        }
+			        if((i%2)==1)
+			        {
+			        	if(!((words[i].equals("+")||(words[i].equals("-"))||(words[i].equals("*"))||(words[i].equals("/"))))){
+			        		System.err.println(i + words[i] + "Некорректный ввод, на месте знака операции иной символ"); 
+			                	throw new ArithmeticException();
+			            	}
+			         }
+			         if(i==(words.length-1)){
+			         	System.err.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)"); 
+			                throw new ArithmeticException();
+			         }
+		        }
+	        try {
+		        Integer i1 = Integer.valueOf(words[0]);
+		        Integer i2 = Integer.valueOf(words[2]);
+		        if(i1<0 || i1>10 || i2<0 || i2>10)
+		        {
+				System.err.println("Неправильный формат строки!");  
+		        	throw new ArithmeticException();
+		        }
+		        switch(words[1]){
+		        	case "+":
+		               		return (i1+i2);
+		                
+		        	case "-":
+		               		return (i1-i2);
+	
+		           	case "/":
+		               		if(i2==0)
+		                    		System.err.println("Делить на ноль нельзя!");  
+		               			return (i1/i2);     
+		           	case "*":
+		               		return (i1*i2);    
+		           	default:
+		           		System.err.println("Неправильный формат строки!");  
+		           		throw new ArithmeticException();   
+		        }
 	        }
-        }
-        catch (NumberFormatException e) {  
-		System.err.println("Неправильный формат строки!"); 
-        	throw new ArithmeticException();
-        }
-    
-
-	}
+	        catch (NumberFormatException e) {  
+			System.err.println("Неправильный формат строки!"); 
+	        	throw new ArithmeticException();
+	        }
+	    
+	
+		}
 	
 }
